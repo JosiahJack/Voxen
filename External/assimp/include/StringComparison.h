fb5c55f02cf6e76b097911d9c8575155db6c1eba
JosiahJack <josiahjackcitadel@gmail.com>
2025-12-25 22:14:15 -0600

    Fix libassimp local dependencies, Fix shader compile error on some older drivers

--- External/assimp/include/StringComparison.h
+++ External/assimp/include/StringComparison.h
@@ -56,8 +56,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma GCC system_header
 #endif
 
-#include <assimp/ai_assert.h>
-#include <assimp/defs.h>
+#include "ai_assert.h"
+#include "defs.h"
 
 #include <cstdint>
 #include <cstring>
--- Shaders/chunk_frag.glsl
+++ Shaders/chunk_frag.glsl
@@ -335,7 +335,7 @@ void main() {
             slopeBias = min(slopeBias, 0.18);
             float bias = slopeBias * (dist / range);
             bias = clamp(bias, 0.0, 0.22);
-            bias += 0.02 * pow(clamp((256.0 - shadSize) / 256.0, 0.0, 1.0), 0.65);
+            bias += 0.02 * pow(clamp((256.0 - float(shadSize)) / 256.0, 0.0, 1.0), 0.65);
             float shadSizeLessOne = float(shadSize - 1);
             // Pseudo-Stochastic PCF sampling
             float sum = 0.0;
